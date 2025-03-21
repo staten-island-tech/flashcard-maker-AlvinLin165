cbabdc08fdfaeb3b7802e53d719a3718e178c66c
AlvinLin165 <alvinlin165@gmail.com>
2025-03-21 09:56:27 -0400

    utkrr7
    iyltti

--- deividissomadatmebecauseIprintnumberthataskuserimsorry.py
+++ deividissomadatmebecauseIprintnumberthataskuserimsorry.py
@@ -1,3 +1,27 @@
-y = input(print("alright?"))
-break diddy
-print(y)
+
+""" class Calculator():
+    def add(x,y):
+        print(x + y)
+        return x + y
+    def add_many(list):
+        print(sum(list))
+        return sum(list)
+    def subtract(list):
+        return list
+Calculator.add() """
+
+class Merchant:
+    def _init_(self, name, products):
+        self.name = name
+        self.products = products
+    def sell(self, item):
+        self.products.remove(item)
+        print(self.products)
+Joanna = Merchant("Joanna", ['Chicken', 'prok', 'beef'])
+Joanna.sell('prok')
+
+
+
+
+
+
