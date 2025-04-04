39548c9fbd8593ac64e510124b01f1702a1ef74f
AlvinLin165 <alvinlin165@gmail.com>
2025-04-04 10:17:32 -0400

    kyy
    iuyfdti7i7i7ti7tty7ttttytyt y

--- deividissomadatmebecauseIprintnumberthataskuserimsorry.py
+++ deividissomadatmebecauseIprintnumberthataskuserimsorry.py
@@ -28,52 +28,41 @@ import json
 class Teacher:
 
     def make_cards():
-        want = input("want make cards? ")
-
-
-        if want == "yes":
-            y = input("quesiton: ")
-            g = input("answer: ")
-            p = {
-                "quesiton": y,
-                "answer": g
-            }
-        else:
-            print("no more")
-
-
-        while want == "yes":
-            want = input("want to make? ")
+        p = {}
+        while True:
+            want = input("want make cards? ")
             if want == "yes":
                 y = input("quesiton: ")
                 g = input("answer: ")
-
+                p[y] = g
             else:
-                print("ok stop")
-
+                print("no more")
+                break
         with open("FlashCards.json", "w") as file:
-                json.dump(p, file, indent = 4)
+            json.dump(p, file, indent = 4)
 
 Teacher.make_cards()
 
-
 class Student:
-    
+
     def answer_question():
+        r = []
         s = 0
-        with open("FlashCards.json") as file:
+        with open("FlashCards.json", "r") as file:
             d = json.load(file)
 
-        for quesiton in d:
-            print(quesiton)
+        for card in d:
+            print(card)
             k = input("what the answer: ")
 
-            if k == k:
+            if k == card:
                 print("correct")
                 s += 1
                 print(f"streaK: {s}")
+
             else:
                 print("wrong")
+                print(f"streak = {s}, now rested to 0")
                 s = 0 
                 
 Student.answer_question()
